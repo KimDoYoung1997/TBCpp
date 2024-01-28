0180c85a95156f5056c38cb84f63e4a2dcaef288
KimDoYoung <ehdud4520@naver.com>
2024-01-28 09:42:33 +0000

    Inline func

---
+++ cpp-examples/hong_lab/chapter_7/lec_7.6/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+inline int min(int x , int y)
+{
+
+    return x > y ? y : x ;
+}
+
+
+int main()
+{
+
+    cout << min(5,6) << endl;
+    cout << min(3,2) << endl;
+
+
+    cout << (5>6 ? 6:5) << endl;
+    cout << (3 > 2 ? 2: 3) << endl;
+
+
+    return 0;
+}
