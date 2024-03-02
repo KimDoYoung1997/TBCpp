c80045c516e97eaf37b29bc9d70dfd1961aad8a8
KimDoYoung1997 <ehdud4520@naver.com>
2024-03-02 15:59:57 +0900

    Coposition relation class

--- cpp-examples/hong_lab/chapter_10/lec_10.2/Monster.h
+++ cpp-examples/hong_lab/chapter_10/lec_10.2/Monster.h
@@ -1,3 +1,11 @@
+/*
+https://chat.openai.com/share/a4c8cf4b-61b8-4d2e-9603-232ec6ba964f
+Composition(구성 요소) 클래스
+- 전체 / 부품의 관계
+- 다른 클래스에는 속할 수 없음
+- 멤버의 존재를 클래스가 관리함
+- 단방향 방향성을 가짐
+*/
 #pragma once
 #include <string>
 #include "Position2D.h"
