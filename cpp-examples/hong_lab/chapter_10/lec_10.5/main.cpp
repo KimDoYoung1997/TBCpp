db0ac88d017d3b7f362b736eafc7881f706390f9
KimDoYoung1997 <ehdud4520@naver.com>
2024-03-07 22:17:04 +0900

    first Timer.h header file

--- cpp-examples/hong_lab/chapter_10/lec_10.5/main.cpp
+++ cpp-examples/hong_lab/chapter_10/lec_10.5/main.cpp
@@ -11,22 +11,7 @@
 #include <chrono>
 #include <ratio>
 
-class Timer
-{
-private:
-    using clock_t = std::chrono::high_resolution_clock;
-    using second_t = std::chrono::duration<double,std::ratio<1>>;
-    std::chrono::time_point<clock_t> start_time = clock_t::now();
 
-public:
-    void elapsed()
-    {
-        std::chrono::time_point<clock_t> end_time = clock_t::now();
-        std::cout << std::chrono::duration_cast<second_t>(end_time - start_time).count() << std::endl;
-        
-    }
-
-};
 
 class Worker
 {
