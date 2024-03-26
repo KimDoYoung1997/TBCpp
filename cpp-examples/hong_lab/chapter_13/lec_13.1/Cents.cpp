52c474ed08091eadb6651f3bc89e38cab3e9dbd6
KimDoYoung1997 <ehdud4520@naver.com>
2024-03-26 23:47:45 +0900

    주석 수정 / 동적 라이브러리 추가

--- cpp-examples/hong_lab/chapter_13/lec_13.1/Cents.cpp
+++ cpp-examples/hong_lab/chapter_13/lec_13.1/Cents.cpp
@@ -2,11 +2,13 @@
 #include "Cents.h"
 using namespace std;
 
-
+// 생성자 정의
 Cents::Cents(int cents) {m_cents = cents;}
 
+// getCents 함수 정의
 int Cents::getCents() const {return m_cents;}
 
+// > 연산자 정의
 bool operator > (const Cents& c1, const Cents& c2)
 {
     return (c1.m_cents > c2.m_cents);
--- cpp-examples/hong_lab/chapter_13/lec_13.1/Cents.h
+++ cpp-examples/hong_lab/chapter_13/lec_13.1/Cents.h
@@ -10,10 +10,12 @@ private:
 public:
 	Cents(int cents);
 
+    // getCents함수 선언
     int getCents() const ;
 
 
 //추가 1
+// << 연산자 선언 및 정의
 friend std::ostream& operator << (std::ostream& out, const Cents& cents)
 {
      out << cents.m_cents << "cents";
@@ -21,6 +23,7 @@ friend std::ostream& operator << (std::ostream& out, const Cents& cents)
 }
 
 //추가 2	
+// > 연산자 선언
 friend bool operator > (const Cents& c1, const Cents& c2);
 // {
 // 	return (c1.m_cents > c2.m_cents);
