8d9b5a547c4619b86ecb5165cc0e2afd8b161cc7
KimDoYoung1997 <ehdud4520@naver.com>
2024-07-15 23:55:18 +0900

    first commit

---
+++ cpp-examples/hong_lab/chapter_19/lec_19.7/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <utility>  // std::forward
+
+struct MyStruct
+{
+
+};
+
+void func(MyStruct&s)
+{
+    std::cout<<"Pass by L-ref"<<std::endl;
+}
+void func(MyStruct&&s)
+{
+    std::cout<<"Pass by R-ref"<<std::endl;
+}
+// void func(MyStruct s)
+// {
+//     std::cout<<"Pass by value"<<std::endl;
+// }
+
+template<typename T>
+void func_wrapper(T t)
+{
+    func(t);
+}
+
+int main()
+{
+    std::cout<<"일반 함수 에서는 L-value와 R-value를 구분하여 오버로딩한다. "<<std::endl;
+    MyStruct s;
+    func(s);
+    func(MyStruct());
+    std::cout<<"Template에서는 L-value와 R-value를 구분하지 않는다. "<<std::endl;
+    func_wrapper(s);
+    func_wrapper(MyStruct());
+
+    return 0;
+}
